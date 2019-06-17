ea5c5b8a04ca762aee320033fde5354d7d9350d6
Bobby BuffaloBoy <novenarii@gmail.com>
2019-06-17 01:15:58 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -2,16 +2,16 @@ nums = [1, 2, 3, 4, 5]
 
 def my_select(array)
   i =0
-  select = []
+  newArray = []
   while i < array.length
     if (yield(array[i]))
-      select << array[i]
+      newArray << array[i]
     end
     i += 1
   end
-  select 
+  newArray
 end
 
-my_select(nums) do |num|
-  num.even?
+my_select(nums) do |x|
+  x.even?
 end
