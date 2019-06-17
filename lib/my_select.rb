71f96444f0cd4e906fd7943c6a438ef7b34a4868
Bobby BuffaloBoy <novenarii@gmail.com>
2019-06-17 01:14:04 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,9 +1,20 @@
+nums = [1, 2, 3, 4, 5]
+
 def my_select(collection)
- i = 0
- newArray = []
- while collection[i] == false
-   newArray.push yield(collection[i])
-   i += 1
- end
- newArray
+  newArray = []
+
+  i=0
+  while i < collection.length
+  newArray.push yield(collection[i])
+  i += 1
+end
+
+  neArray.compact
+end
+
+
+my_select(nums) do |x| 
+  if x.even?
+    num
+  end
 end
